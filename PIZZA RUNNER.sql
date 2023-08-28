28c376935321107aa15771cff9d70f9befa656d2
ChisomNwankwo <cynthiarapheals@gmail.com>
2023-08-28 14:24:51 +0100

    update script

--- PIZZA RUNNER.sql
+++ PIZZA RUNNER.sql
@@ -217,3 +217,13 @@ FROM pizza_runner.customer_orders;
 SELECT DAYNAME(order_time) AS day_of_week, COUNT(pizza_id) 'Volume of Pizza Ordered' -- extract works on mysql
 FROM pizza_runner.customer_orders
 GROUP BY day_of_week;
+
+## B. Runner and Customer Experience
+-- Question 1: How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+
+-- Question 2: What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+-- Question 3: Is there any relationship between the number of pizzas and how long the order takes to prepare?
+-- Question 4: What was the average distance travelled for each customer?
+-- Question 5: What was the difference between the longest and shortest delivery times for all orders?
+-- Question 6: What was the average speed for each runner for each delivery and do you notice any trend for these values?
+-- Question 7: What is the successful delivery percentage for each runner?

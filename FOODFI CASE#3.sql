1a168a1436cd89d75391cce96f9c97bb45d97ce0
ChisomNwankwo <cynthiarapheals@gmail.com>
2023-09-09 22:51:19 +0100

    add new file

---
+++ FOODFI CASE#3.sql
@@ -0,0 +1,29 @@
+--CREATE DATABASE
+CREATE SCHEMA foodie_fi;
+SET search_path = foodie_fi;
+
+CREATE TABLE plans (
+  plan_id INTEGER,
+  plan_name VARCHAR(13),
+  price DECIMAL(5,2)
+);
+
+INSERT INTO plans
+  (plan_id, plan_name, price)
+VALUES
+  ('0', 'trial', '0'),
+  ('1', 'basic monthly', '9.90'),
+  ('2', 'pro monthly', '19.90'),
+  ('3', 'pro annual', '199'),
+  ('4', 'churn', null);
+
+
+
+CREATE TABLE subscriptions (
+  customer_id INTEGER,
+  plan_id INTEGER,
+  start_date DATE
+);
+
+-- B. Data Analysis Questions
+-- How many customers has Foodie-Fi ever had?
